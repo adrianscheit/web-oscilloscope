7f679c8f5df13ae4001e20583d6607e18d1f1309
Adrian Scheit <adrianscheit@gmail.com>
2025-02-08 00:10:55 +0100

    Add Y-scale that does not work yet

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # WEB Oscilloscope
+
+https://adrianscheit.github.io/web-oscilloscope/
+
 Simple and limited oscilloscope implementation using WEB Audio API.
--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 const paused = document.getElementById('paused') as HTMLInputElement;
+const yScale = document.getElementById('paused') as HTMLInputElement;
 const audioCtx = new AudioContext();
+const colors = ['#f004', '#0f04', '#00f4', '#ff04'];
 
 class Analyzer {
-    static readonly fftSize = 4096;
+    static readonly fftSize = 2048;
 
     readonly analyzer: AnalyserNode = audioCtx.createAnalyser();
     readonly data = new Uint8Array(Analyzer.fftSize);
@@ -31,8 +33,6 @@ class Analyzer {
 navigator.mediaDevices.getUserMedia({ audio: { deviceId: undefined } }).then((mediaStream: MediaStream) => {
     const source = audioCtx.createMediaStreamSource(mediaStream);
     const splitter = audioCtx.createChannelSplitter(source.channelCount);
-    console.log(source, splitter);
-    const colors = ['#f004', '#0f04', '#00f4', '#ff04']
     const analysers: Analyzer[] = Array(source.channelCount)
         .fill(undefined)
         .map((_, index) => new Analyzer(colors[index % 4]));
@@ -42,30 +42,42 @@ navigator.mediaDevices.getUserMedia({ audio: { deviceId: undefined } }).then((me
     source.connect(splitter);
     console.log(source, splitter, analysers);
 
+    yScale.addEventListener('change', () => {
+        const value = +yScale.value;
+        analysers.forEach((it) => it.analyzer.maxDecibels = value);
+    });
+
     const canvas = document.getElementById("oscilloscope") as HTMLCanvasElement;
     canvas.width = Analyzer.fftSize;
-    canvas.height = 256;
+    canvas.height = 300;
     const canvasCtx = canvas.getContext("2d")!;
-    canvasCtx.fillStyle = "#ddd";
-    canvasCtx.lineWidth = 1;
+    canvasCtx.font = '24px sans-serif'
+    canvasCtx.fillStyle = "#000";
+    for (let i = 0; i < Analyzer.fftSize; i += 100) {
+        canvasCtx.fillText(`${Math.round(1000 * i / audioCtx.sampleRate)}ms`, i, 300);
+    }
 
+    canvasCtx.fillStyle = "#eee";
+    canvasCtx.lineWidth = 1;
     function draw(): void {
         if (!paused.checked) {
             analysers.forEach((analyser) => analyser.getData());
-            canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+            canvasCtx.fillRect(0, 0, canvas.width, 256);
             analysers.forEach((analyser) => analyser.draw(canvasCtx));
             requestAnimationFrame(draw);
         }
     }
-    audioCtx.resume();
-    draw();
 
-    paused.addEventListener('change', () => {
+    const pausedHandler = () => {
         if (paused.checked) {
             audioCtx.suspend();
         } else {
             audioCtx.resume();
             draw();
         }
-    });
+    };
+
+    paused.addEventListener('change', () => pausedHandler());
+
+    pausedHandler();
 });
