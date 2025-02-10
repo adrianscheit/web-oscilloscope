360463d81b67a54d00c5da3e97fe12524f5a9889
Adrian Scheit <adrianscheit@gmail.com>
2025-02-10 17:50:02 +0100

    Improve info

--- main.ts
+++ main.ts
@@ -2,8 +2,8 @@ const paused = document.getElementById('paused') as HTMLInputElement;
 const yScale = document.getElementById('yScale') as HTMLInputElement;
 const fftSizeInput = document.querySelector('[name=fftSize]') as HTMLInputElement;
 const xStabilization = document.querySelector('[name=xStabilization]') as HTMLInputElement;
-const audioCtx = new AudioContext();
-audioCtx.suspend();
+const audioContext = new AudioContext();
+audioContext.suspend();
 const colors = ['#f00', '#0f0', '#00f', '#aa0', '#0aa', '#a0a'];
 const infoText: Text = document.getElementById('info')!.appendChild(document.createTextNode(''));
 
@@ -32,8 +32,8 @@ class Analyzer {
     static readonly xStabilization: number = parseInt(xStabilization.value);
     static readonly maxShift = this.fftSize / this.xStabilization;
 
-    readonly gain: GainNode = audioCtx.createGain();
-    readonly analyzer: AnalyserNode = audioCtx.createAnalyser();
+    readonly gain: GainNode = audioContext.createGain();
+    readonly analyzer: AnalyserNode = audioContext.createAnalyser();
     readonly data = new Uint8Array(Analyzer.fftSize);
     readonly fftData: Uint8Array;
     readonly gainControl = document.createElement('label');
@@ -41,21 +41,11 @@ class Analyzer {
     constructor(private readonly strokeStyle: string) {
         this.analyzer.fftSize = Analyzer.fftSize;
         this.analyzer.smoothingTimeConstant = 0;
-        this.analyzer.channelCount = 1;
         this.fftData = new Uint8Array(this.analyzer.frequencyBinCount);
         this.gain.channelCount = 1;
         this.gain.connect(this.analyzer);
 
-        this.gainControl.style.color = strokeStyle;
-        this.gainControl.appendChild(document.createTextNode('yScale'));
-        const input = document.createElement('input');
-        input.type = 'range';
-        input.min = '1';
-        input.max = '10';
-        input.step = '0.1';
-        input.value = '1';
-        this.gainControl.appendChild(input);
-        input.addEventListener('change', () => this.gain.gain.value = +input.value);
+        this.createGainControl();
     }
 
     getData(): void {
@@ -75,41 +65,66 @@ class Analyzer {
         return 0;
     }
 
-    draw(canvasCtx: CanvasRenderingContext2D): void {
-        canvasCtx.beginPath();
-        canvasCtx.strokeStyle = this.strokeStyle;
+    draw(ctx: CanvasRenderingContext2D): void {
+        ctx.fillRect(0, 0, ctx.canvas.width, 256);
+        ctx.beginPath();
+        ctx.strokeStyle = this.strokeStyle;
         const shift = this.calc0Shift();
-        canvasCtx.moveTo(0, this.data[shift]);
+        ctx.moveTo(0, this.data[shift]);
         for (let i = shift + 1; i < this.data.length; i++) {
-            canvasCtx.lineTo(i - shift, this.data[i]);
+            ctx.lineTo(i - shift, this.data[i]);
         }
-        canvasCtx.stroke();
+        ctx.stroke();
     }
 
-    drawFft(canvasCtx: CanvasRenderingContext2D): void {
-        let max = 0, maxI = 0;
-        canvasCtx.beginPath();
-        canvasCtx.strokeStyle = this.strokeStyle;
-        canvasCtx.moveTo(0, 255 - this.fftData[0]);
+    drawFft(ctx: CanvasRenderingContext2D): void {
+        let maxI = 0, max = this.fftData[maxI];
+        ctx.fillRect(0, 0, ctx.canvas.width, 256);
+        ctx.beginPath();
+        ctx.strokeStyle = this.strokeStyle;
+        ctx.moveTo(0, 255 - this.fftData[0]);
         for (let i = 1; i < this.fftData.length; i++) {
-            canvasCtx.lineTo(i, 255 - this.fftData[i]);
+            ctx.lineTo(i, 255 - this.fftData[i]);
             if (this.fftData[i] > max) {
                 max = this.fftData[i];
                 maxI = i;
             }
         }
-        canvasCtx.stroke();
-        const previousFillStyle = canvasCtx.fillStyle;
-        canvasCtx.fillStyle = this.strokeStyle;
-        canvasCtx.fillText(`${(maxI + 0.5) * audioCtx.sampleRate / Analyzer.fftSize}Hz`, maxI, 20);
-        canvasCtx.fillStyle = previousFillStyle;
+        ctx.stroke();
+        const previousFillStyle = ctx.fillStyle;
+        ctx.fillStyle = this.strokeStyle;
+        ctx.fillText(`${(maxI + 0.5) * audioContext.sampleRate / Analyzer.fftSize}Hz`, maxI, 20);
+        ctx.fillStyle = previousFillStyle;
+    }
+
+    private createGainControl(): void {
+        this.gainControl.style.color = this.strokeStyle;
+        this.gainControl.appendChild(document.createTextNode('GAIN'));
+        const input = this.gainControl.appendChild(document.createElement('input'));
+        input.type = 'range';
+        input.min = '1';
+        input.max = '10';
+        input.step = '0.1';
+        input.value = '1';
+        input.addEventListener('change', () => this.gain.gain.value = +input.value);
     }
 }
 
+const drawXInfo = (ctx: CanvasRenderingContext2D, x: number, text: string): boolean => {
+    ctx.fillRect(x, 256, 1, 300 - 256);
+    ctx.fillText(text, x + 1, 300 - 1);
+    return x < ctx.canvas.width;
+};
+
 navigator.mediaDevices.getUserMedia({ audio: { deviceId: undefined } }).then((mediaStream: MediaStream) => {
-    const source = audioCtx.createMediaStreamSource(mediaStream);
-    infoText.nodeValue = `There is audio soruce of ${source.numberOfOutputs} outputs and ${source.channelCount} channels`;
-    const splitter = audioCtx.createChannelSplitter(source.channelCount);
+    const source = audioContext.createMediaStreamSource(mediaStream);
+    infoText.nodeValue = `There is audio soruce of:
+        ${source.numberOfOutputs} outputs,
+        ${source.channelCount} channels,
+        ${audioContext.sampleRate} sample rate,
+        ${1000 * Analyzer.fftSize / audioContext.sampleRate}ms oscilloscope view width
+        `;
+    const splitter = audioContext.createChannelSplitter(source.channelCount);
     const analysers: Analyzer[] = Array(source.channelCount)
         .fill(undefined)
         .map((_, index) => new Analyzer(colors[index % 4]));
@@ -132,24 +147,18 @@ navigator.mediaDevices.getUserMedia({ audio: { deviceId: undefined } }).then((me
     canvasCtx.font = '20px sans-serif';
     canvasCtx.fillStyle = "#000";
     for (let i = 0; true; i += 1) {
-        const x = i * audioCtx.sampleRate / 1000 + Analyzer.dataMiddleIndex;
-        if (x >= canvas.width) {
+        const x = i * audioContext.sampleRate / 1000 + Analyzer.dataMiddleIndex;
+        if (!drawXInfo(canvasCtx, x, i % 5 ? '' : `${i}ms`)) {
             break;
-        }
-        canvasCtx.fillRect(x, 256, 1, 300 - 256);
-        if (i % 5 === 0) {
-            canvasCtx.fillText(`${i}ms`, x + 1, 300);
         }
     }
     fftCanvasCtx.font = '20px sans-serif';
     fftCanvasCtx.fillStyle = "#000";
     for (let i = 0; true; i += 1) {
-        const x = 1000 * i * Analyzer.fftSize / audioCtx.sampleRate;
-        if (x >= fftCanvas.width) {
+        const x = 1000 * i * Analyzer.fftSize / audioContext.sampleRate;
+        if (!drawXInfo(fftCanvasCtx, x, `${i}kHz`)) {
             break;
         }
-        fftCanvasCtx.fillRect(x, 256, 1, 300 - 256);
-        fftCanvasCtx.fillText(`${i}kHz`, x + 1, 300);
     }
 
     canvasCtx.fillStyle = "#eee";
@@ -157,21 +166,23 @@ navigator.mediaDevices.getUserMedia({ audio: { deviceId: undefined } }).then((me
     canvasCtx.lineWidth = 1;
     function draw(): void {
         if (!paused.checked) {
-            analysers.forEach((analyser) => analyser.getData());
-            canvasCtx.fillRect(0, 0, Analyzer.fftSize, 256);
-            analysers.forEach((analyser) => analyser.draw(canvasCtx));
-            analysers.forEach((analyser) => analyser.getFftData());
-            fftCanvasCtx.fillRect(0, 0, Analyzer.fftSize, 256);
-            analysers.forEach((analyser) => analyser.drawFft(fftCanvasCtx));
+            analysers.forEach((analyser) => {
+                analyser.getData();
+                analyser.getFftData();
+            });
+            analysers.forEach((analyser) => {
+                analyser.draw(canvasCtx);
+                analyser.drawFft(fftCanvasCtx);
+            });
             requestAnimationFrame(draw);
         } else {
-            audioCtx.suspend();
+            audioContext.suspend();
         }
     }
 
     const pausedHandler = () => {
         if (!paused.checked) {
-            audioCtx.resume();
+            audioContext.resume();
             draw();
         }
     };
